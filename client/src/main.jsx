c59fad66a3df32e6b75d27d5eaf76c0facbf238f
kshitijmohankar-7 <kshitijmohankar7@gmail.com>
2026-09-15 22:47:40 +0530

    feat: enable toast provider globally

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import { ThemeProvider } from './context/ThemeContext';
+import { ToastProvider } from './context/ToastContext';
 import './index.css';
 import './styles/modern.css';
 import './styles/dashboard.css';
@@ -11,13 +12,16 @@ import './styles/immersive-shell.css';
 import './styles/feature-menu.css';
 import './styles/menu-only-shell.css';
 import './styles/leave.css';
+import './styles/advanced-features.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider>
         <AuthProvider>
-          <App />
+          <ToastProvider>
+            <App />
+          </ToastProvider>
         </AuthProvider>
       </ThemeProvider>
     </BrowserRouter>
